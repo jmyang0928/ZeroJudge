3c4589cd48eab970d1ad90d49f7e4f4ab38455fc
jimmyyang0928 <55872381+jimmyyang0928@users.noreply.github.com>
2022-06-04 12:25:59 +0800

    d887 AC

--- d887/d887.cpp
+++ d887/d887.cpp
@@ -1,13 +1,17 @@
+// AC (2ms, 360KB)
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int N;
+vector<vector<long long>> dp(51,vector<long long>(26,-1));
 long long func(int cur,int umd){
-    if(cur==N*2 && umd==0) return 1;
     if(umd<0) return 0;
-    if(N*2-umd==cur) return 1;
-    if(N*2-umd<cur) return 0;
-    return func(cur+1,umd+1)+func(cur+1,umd-1);
+    if(dp[50-N*2+cur][umd]!=-1) return dp[50-N*2+cur][umd];
+    if(cur==N*2 && umd==0) return dp[50-N*2+cur][umd]=1;
+    if(N*2-umd==cur) return dp[50-N*2+cur][umd]=1;
+    if(N*2-umd<cur) return dp[50-N*2+cur][umd]=0;
+    return dp[50-N*2+cur][umd]=(func(cur+1,umd+1)+func(cur+1,umd-1));
 }
 int main(){
     ios_base::sync_with_stdio(false);
