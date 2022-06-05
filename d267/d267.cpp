78cecf0b212bc5ca7f1a202c5251e387588f6049
jimmyyang0928 <55872381+jimmyyang0928@users.noreply.github.com>
2022-06-05 11:40:44 +0800

    d267 AC_2

--- d267/d267.cpp
+++ d267/d267.cpp
@@ -4,19 +4,18 @@
 #include<algorithm>
 using namespace std;
 
-int N;
-string input;
-int maxUsage;
+int N;          //N:紀錄測資筆數。
+string input;   //input:儲存當筆測資。
+int maxUsage;   //maxUsage:紀錄最大使用量的數值。
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
     cin>>N;
-    cin.ignore();
+    cin.ignore();   //cin後使用getline前要加上cin.ignore()。
     while(N--){
         maxUsage=0;
         vector<int> usage(26,0);
-        getline(cin,input);
-        sort(input.begin(),input.end());
+        getline(cin,input);     //讀入一行測資。
         for(char c:input){
             if(c>='a' && c<='z'){
                 usage[c-'a']++;
