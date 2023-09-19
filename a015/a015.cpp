f64847e89d031a0d43fc970c54f2ed97f2889b4a
jmyang0928 <yangjingmin928@gmail.com>
2023-09-19 11:31:27 +0800

    a015 AC

---
+++ a015/a015.cpp
@@ -0,0 +1,31 @@
+// AC (3ms, 340KB)
+// Author: JMYANG
+// Tag: array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m, n;
+    while (cin >> m >> n)
+    {
+        int a[m][n];
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cin >> a[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cout << a[j][i] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
